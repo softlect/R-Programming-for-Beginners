52f2492b9ad11854950a8c5c1d40b2e096ce6263
softlect <64558769+softlect@users.noreply.github.com>
2020-04-30 23:27:59 +0530

    Delete Others1.R

--- 6. Matrices/Others1.R
+++
@@ -1,9 +0,0 @@
-a <- c(316,556)
-b <- c(343,584)
-c <- c(378,742)
-earnings <- rbind(a,b,c)
-colnames(earnings) <- c('US','Non-US')
-rownames(earnings) <- c('Star Wars','Skyfall','Avengers')
-earnings
-earnings_euros <- earnings / 1.12
-earnings_euros
